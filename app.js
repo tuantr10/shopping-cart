2dd71ed1212f4cefcb7d61bc0d242f9f82a26f7e
Tuan Dung TRAN <mcafee92@gmail.com>
2018-03-04 23:40:57 +0900

    add bower/bootstrap

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	let err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handler
