1b1b999cee312e9d21f153626935ed2208593447
Tuan Dung TRAN <mcafee92@gmail.com>
2018-03-25 04:10:18 +0900

    temprorary manage products using json data type

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,6 @@
 let index = (req, res, next) => {
   let masterData = require('../masterdata');
+  console.log(masterData);
   res.render('index', { title: 'Viet Tran Yen', data: masterData });
 };
 
---
+++ masterdata.js
@@ -0,0 +1,62 @@
+let data = {
+  cleanProducts: [{
+    name: 'Yến Sạch A - 100g',
+    new: true,
+    sale: '20%',
+    oldPrice: 45.00,
+    price: 32.00,
+    image: './img/yen-sao-a.jpg'
+  }, {
+    name: 'Yến Sạch B - 100g',
+    new: true,
+    sale: '20%',
+    oldPrice: 45.00,
+    price: 32.00,
+    image: './img/yen-sao-b.jpg'
+  }, {
+    name: 'Yến Sạch C - 100g',
+    new: true,
+    sale: '20%',
+    oldPrice: 45.00,
+    price: 32.00,
+    image: './img/yen-sao-c.jpg'
+  }, {
+    name: 'Yến Sạch A - 10g',
+    new: true,
+    sale: '20%',
+    oldPrice: 45.00,
+    price: 32.00,
+    image: './img/yen-10g.jpg'
+  }],
+  rawProducts: [{
+    name: 'Yến Thô A - 100g',
+    new: true,
+    sale: '20%',
+    oldPrice: 45.00,
+    price: 32.00,
+    image: './img/yen-tho-a.jpg'
+  }, {
+    name: 'Yến Thô B - 100g',
+    new: true,
+    sale: '20%',
+    oldPrice: 45.00,
+    price: 32.00,
+    image: './img/yen-tho-b.jpg'
+  }, {
+    name: 'Yến Thô A - 10g',
+    new: true,
+    sale: '20%',
+    oldPrice: 45.00,
+    price: 32.00,
+    image: './img/yen-sao-c.jpg'
+  }, {
+    name: 'Yến Thô B - 10g',
+    new: true,
+    sale: '20%',
+    oldPrice: 45.00,
+    price: 32.00,
+    image: './img/yen-10g.jpg'
+  }]
+};
+
+module.exports = data;
