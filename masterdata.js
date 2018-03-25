74fab04951c55458babc9b9b85aa76dfa3d2d179
Tuan Dung TRAN <mcafee92@gmail.com>
2018-03-26 00:55:55 +0900

    tweak products page

--- masterdata.js
+++ masterdata.js
@@ -3,58 +3,58 @@ let data = {
     name: 'Yến Sạch A - 100g',
     new: true,
     sale: '20%',
-    oldPrice: 45.00,
-    price: 32.00,
+    oldPrice: 45.000,
+    price: '32.000',
     image: './img/yen-sao-a.jpg'
   }, {
     name: 'Yến Sạch B - 100g',
     new: true,
     sale: '20%',
-    oldPrice: 45.00,
-    price: 32.00,
+    oldPrice: 45.000,
+    price: '32.000',
     image: './img/yen-sao-b.jpg'
   }, {
     name: 'Yến Sạch C - 100g',
     new: true,
     sale: '20%',
-    oldPrice: 45.00,
-    price: 32.00,
+    oldPrice: 45.000,
+    price: '32.000',
     image: './img/yen-sao-c.jpg'
   }, {
     name: 'Yến Sạch A - 10g',
     new: true,
     sale: '20%',
-    oldPrice: 45.00,
-    price: 32.00,
+    oldPrice: 45.000,
+    price: '32.000',
     image: './img/yen-10g.jpg'
   }],
   rawProducts: [{
     name: 'Yến Thô A - 100g',
     new: true,
     sale: '20%',
-    oldPrice: 45.00,
-    price: 32.00,
+    oldPrice: 45.000,
+    price: '32.000',
     image: './img/yen-tho-a.jpg'
   }, {
     name: 'Yến Thô B - 100g',
     new: true,
     sale: '20%',
-    oldPrice: 45.00,
-    price: 32.00,
+    oldPrice: 45.000,
+    price: '32.000',
     image: './img/yen-tho-b.jpg'
   }, {
     name: 'Yến Thô A - 10g',
     new: true,
     sale: '20%',
-    oldPrice: 45.00,
-    price: 32.00,
+    oldPrice: 45.000,
+    price: '32.000',
     image: './img/yen-sao-c.jpg'
   }, {
     name: 'Yến Thô B - 10g',
     new: true,
     sale: '20%',
-    oldPrice: 45.00,
-    price: 32.00,
+    oldPrice: 45.000,
+    price: '32.000',
     image: './img/yen-10g.jpg'
   }]
 };
